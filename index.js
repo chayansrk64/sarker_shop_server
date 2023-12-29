65c0a6c025cfecd868c2d4aa71cc74a0d62609c8
chayansrk64 <chayansrk64@gmail.com>
2023-12-30 02:31:14 +0600

    verify jwt middleware added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -10,6 +11,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verifyJWT
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+       return res.status(401).send({error: true, message:"unauthorized access"})
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({error: true, message:"unauthorized access"})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -40,6 +58,13 @@ async function run() {
     const userCollection = client.db('sarker_shopDB').collection('users');
 
 
+
+    // JWT 
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res.send({ token });
+    })
 
 
     // users api
@@ -92,11 +117,17 @@ async function run() {
 
     // cart collection api
 
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT, async(req, res) => {
         const email = req.query.email;
         if(!email){
            res.send([])
         }
+
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+          return res.status(403).send({error: true, message:"forbidden access"})
+        }
+
         const query = {email: email}
         const result = await cartCollection.find(query).toArray();
         res.send(result);
